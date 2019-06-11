a7e6e107f19f0dd8375225b51de4cf712b44e230
zamarawka <somebody.told@mail.ru>
2019-06-11 14:18:44 +0300

    Reduce bot angriness on requests.

--- app/middlewares/isAngryBot.js
+++ app/middlewares/isAngryBot.js
@@ -1,12 +1,16 @@
 const sample = require('lodash/sample');
 const readYaml = require('read-yaml');
 
+const { isAngry } = require('../utils');
+
 const { angryBot } = readYaml.sync('./i18n/ru/request.yml');
+
+const { BOT_DAMN_RATE } = process.env;
 
 const preveledgeMention = /^уважаемый/i;
 
 module.exports = ({ currentUser, reply, message }, next) => {
-  if (currentUser.isAnnoy && !preveledgeMention.test(message.text)) {
+  if (currentUser.isAnnoy && isAngry(BOT_DAMN_RATE) && !preveledgeMention.test(message.text)) {
     return reply(sample(angryBot));
   }
 
